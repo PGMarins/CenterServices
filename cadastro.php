998a1d40f015eb497cb6e3c4fb44e2a197388736
Unknown <paulogabrielmarins@outlook.com>
2018-07-06 01:25:33 -0300

    alteração de cadastro

--- cadastro.php
+++ cadastro.php
@@ -6,7 +6,7 @@ include_once("conecta.php");
 	$email=$_POST['email'];
 	$senha=md5($_POST['senha']);
 
-	$sql="insert into usuarios (nome,sobrenome,email,senha,id_grupo) values('$primeiroNome','$sobrenome','$email','$senha','2')";//comando sql
+	$sql="insert into usuarios (nome,sobrenome,email,senha) values('$primeiroNome','$sobrenome','$email','$senha')";//comando sql
 	$salvar=mysqli_query($conexao,$sql);//executa o comando sql
 	if(mysqli_affected_rows($conexao)==1)header("location:login.php");//verifica se cadastrou ou nao
 	else header("location:cria.html");
